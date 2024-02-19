b9bb9f9d93b8e15c1c98aa49a52388e3a1bb6347
Ariel <user@MacBook-Pro-de-Ariel.local>
2024-02-19 11:46:33 -0500

    js del inicio

---
+++ assets/js/inicio.js
@@ -0,0 +1,19 @@
+if(!sessionStorage.getItem('correo')){
+    window.location.href = './login.html'
+}
+
+//Recuperar y utilizar el valor del LocalStorage
+
+let nombre = document.getElementById('nombre')
+let usuario = localStorage.getItem('nombres')  +' ' + localStorage.getItem('apellidos')
+
+nombre.innerHTML = `Bienvenido <strong> ${usuario}</strong>`
+
+let salir = document.getElementById('salir')
+
+salir.addEventListener('click', cerrar);
+
+function cerrar() {
+    sessionStorage.clear()
+    window.location.href("index.html")
+}
